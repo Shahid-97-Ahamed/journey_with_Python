0ce49c1b20b21870bad9a2209f9801972c66c1b0
Shahid Ahamed <shahid.07ahamed@gmail.com>
2026-04-26 23:03:48 +0900

    basic python

--- codebreadge/firstClass.py
+++ codebreadge/firstClass.py
@@ -1,6 +1,11 @@
-age  =input("input your age:")
+# age  =input("input your age:")
 
 # print("user's age:",age)
 
-question_1 =input("How are you?")
-print("Answer:",question_1)
+# question_1 =input("How are you?")
+# print("Answer:",question_1)
+
+first_name ="アハメド"
+last_name ="シャヒド"
+full_name =first_name +" "+ last_name
+print(full_name)
