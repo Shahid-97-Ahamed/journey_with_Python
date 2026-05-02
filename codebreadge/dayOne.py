c6097ef5f400d5f9e23e9c43f66081de9b8e84c4
Shahid Ahamed <shahid.07ahamed@gmail.com>
2026-05-02 22:02:16 +0900

    Integer

--- codebreadge/dayOne.py
+++ codebreadge/dayOne.py
@@ -10,20 +10,46 @@
 
 # 2. String
 
-city = "Tokyo"
-print(city)
+# city = "Tokyo"
+# print(city)
 
-programming_language ="Python"
-print(programming_language[0],programming_language[-1])
+# programming_language ="Python"
+# print(programming_language[0],programming_language[-1])
 
-name ="Allah"
-print(len(name))
+# name ="Allah"
+# print(len(name))
 
-x ="Abdul"
-# print("".join(reversed(x)))
-# print(x[::-1])
+# x ="Abdul"
+# # print("".join(reversed(x)))
+# # print(x[::-1])
 
-y = "World"
-print("Adul" in y)
+# y = "World"
+# print("Adul" in y)
+
+# 3. Integer
+
+x = 10
+y = 20
+print(x+y)
+
+x = 20
+y = 32
+print(x-y)
+
+x = 32
+y = 55
+print(x/y)
+
+x = 25
+y= 5
+print(x/y)
+
+x = 100
+y = 25
+
+if x % 2 == 0:
+    print("Even Number")
+else:
+    print("ODD Number")
 
 
