e3556a4acc37c20bb07915c355167225b68aa125
Shahid Ahamed <shahid.07ahamed@gmail.com>
2026-05-02 21:28:48 +0900

    print function

---
+++ codebreadge/dayOne.py
@@ -0,0 +1,9 @@
+# 1. Print Function
+# print("Shahid Ahamed")
+# print(28,31.5)
+# print(True,False,None)
+
+# print("Muhammed",520)
+
+# sand_Massage ="Hello!! I am Python,Nice to meet you.........."
+# print(sand_Massage)
