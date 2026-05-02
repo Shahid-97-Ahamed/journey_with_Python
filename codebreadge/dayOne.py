41720b1d9e9590941a59a97bf02aed38dfeb9c28
Shahid Ahamed <shahid.07ahamed@gmail.com>
2026-05-02 21:56:02 +0900

    string

--- codebreadge/dayOne.py
+++ codebreadge/dayOne.py
@@ -7,3 +7,23 @@
 
 # sand_Massage ="Hello!! I am Python,Nice to meet you.........."
 # print(sand_Massage)
+
+# 2. String
+
+city = "Tokyo"
+print(city)
+
+programming_language ="Python"
+print(programming_language[0],programming_language[-1])
+
+name ="Allah"
+print(len(name))
+
+x ="Abdul"
+# print("".join(reversed(x)))
+# print(x[::-1])
+
+y = "World"
+print("Adul" in y)
+
+
