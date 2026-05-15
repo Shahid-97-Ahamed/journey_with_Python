91b53648652b918914129769700c15c796e50833
Shahid Ahamed <shahid.07ahamed@gmail.com>
2026-05-16 00:32:45 +0900

    learn sequence type and bytes type

---
+++ codebreadge/sequence.py
@@ -0,0 +1,11 @@
+# sequence is three type 1. list 2. tuple 3.range
+# list type data
+li =["Ammer","Sadik","Sarzis"]#this is list type ;list value is changable ite also use []
+print(li)
+# tuple type data
+tu =(1,2,156,25556,2)#this is tuple type ;its value is not changable its also use ()
+print(tu)
+# range type data
+ran =range(420)#its means this value range is 0-419
+for x in ran:
+    print(x)
