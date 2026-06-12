a7009cd98bab8986d3e21d3a76ae3f9a84ea13ae
Shahid Ahamed <shahid.07ahamed@gmail.com>
2026-06-12 21:20:48 +0900

    update

--- codebreadge/index.py
+++ codebreadge/index.py
@@ -1,1 +1,14 @@
-print("Hello World")
+cart =[]
+
+i = 0
+
+while i < 5:
+     price = float(input(f"Enter Your Price {i+1}: "))
+     cart.append(price)
+i+=1
+print(cart)
+c =0
+total =0
+
+     
+     
---
+++ index.py
@@ -0,0 +1,65 @@
+# ----------------------------------Python Zero To Advance------------------------------------
+
+# name = "Rahim"
+# age = 20
+# hobbie = "Playing Football"
+# print("My Name is: ",name)
+# print("My age is: ",age)
+# print("My hobbie is: ",hobbie)
+
+
+#------------------------ Input---------------------------
+
+# users_name=input("Enter Your name: ")
+# users_birthYear=int(input("Enter Your Birth Year: "))
+# present_years = 2026
+# age =present_years - users_birthYear
+# print(f"Hello {users_name}! Your age is {age}")
+
+# -------------------If-else condition-----------------------------
+
+# digits = int(input("Input Your degits: "))
+# if digits % 2 == 1:
+#     print(f"{digits} is an Odd number")
+# else:
+#     print(f"This {digits} is Even number")
+
+# students_exam_number =int(input("Enter the Number: "))
+
+# if students_exam_number >= 80:
+#     print("Your grade is: A+")
+# elif students_exam_number >= 70:
+#     print("Your grade is: A")
+# elif students_exam_number >= 60:
+#     print("Your grade is: B")
+# elif students_exam_number >= 50:
+#     print("Your grade is: C")
+# else:
+#     print("Fail")
+
+
+# -------------------------for-loop and range---------------------------------
+
+# for i in range(1,1001):
+#     print(i)
+
+# num =int(input("Enter a number: "))
+
+# for i in range(1,11):
+#     print(f"{num} * {i} = {num * i}")
+
+# -------------------------Accumulator( যোগফল জমানো প্যাটার্ন) ----------------
+
+# total = 0
+# for i in range (1,6):
+#     total += i
+# print(total)
+
+# N =int(input("Enter a number: "))
+
+# total = 0
+# for i in range(1,N+1):
+#     if i % 2 == 0:
+#         total = total + i
+# print(f"Sum of even numbers from 1 to {N} is: {total}")
+
