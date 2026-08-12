47795b7d1cc10e6de375175ac07c8e9ad8d12313
Shahid Ahamed <shahid.07ahamed@gmail.com>
2026-08-12 19:12:11 +0900

    oop start2

---
+++ oop/oop.py
@@ -0,0 +1,27 @@
+# class Airplains:
+#     def __init__(self,name,model,capacity,max_speed):
+#         self.name =name
+#         self.model =model
+#         self.capacity =capacity
+#         self.max_speed =max_speed
+
+# Bg=Airplains("BG616","767",456,215)
+# Jal=Airplains("BG616","767",456,215)
+# dubai=Airplains("BG616","767",456,215)
+
+# print(Jal.capacity)
+
+
+# --------------------------------------------------------------------------
+
+class Students:
+    def __init__(self,name,marks):
+        self.name =name
+        self.marks =marks
+
+    def get_avg(self):
+        total =sum(self.marks)
+        print(f"Hi {self.name},Your avarage markes is {total/len(self.marks):.1f}")
+
+my_result=Students("Shahid",[98,85,45,34])
+my_result.get_avg()
