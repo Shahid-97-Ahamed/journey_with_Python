d8dcbbcb9290f28d3131a5db56f1983cdf0b3a98
Shahid Ahamed <shahid.07ahamed@gmail.com>
2026-08-12 23:08:13 +0900

    # 05.ATM Withdrawal Attempts

--- oop/oop.py
+++ oop/oop.py
@@ -14,52 +14,52 @@
 
 # # --------------------------------------------------------------------------
 
-# class Students:
-#     def __init__(self,name,marks):
-#         self.name =name
-#         self.marks =marks
+# # class Students:
+# #     def __init__(self,name,marks):
+# #         self.name =name
+# #         self.marks =marks
 
-#     def get_avg(self):
-#         total =sum(self.marks)
-#         print(f"Hi {self.name},Your avarage markes is {total/len(self.marks):.1f}")
+# #     def get_avg(self):
+# #         total =sum(self.marks)
+# #         print(f"Hi {self.name},Your avarage markes is {total/len(self.marks):.1f}")
 
-# my_result=Students("Shahid",[98,85,45,34])
-# my_result.get_avg()
+# # my_result=Students("Shahid",[98,85,45,34])
+# # my_result.get_avg()
 
 
-# ---------------------------------------------------------------------------------
+# # ---------------------------------------------------------------------------------
 
-# Basic Fundamentan python Problems
+# # Basic Fundamentan python Problems
 
-# 1.Self Introduction Card
+# # 1.Self Introduction Card
 
 # name ="Shahid"
 # age = 32
 # city ="Kagoshima"
 
-# print(f"My name is {name}, I am {age} years old, and I live in {city}.")
+# # print(f"My name is {name}, I am {age} years old, and I live in {city}.")
 
 
-# 2.Suica Card Balance Check
+# # 2.Suica Card Balance Check
 
-# balance = int(input("Input your balance: "))
+# # balance = int(input("Input your balance: "))
 # train_fare = 410
 
 # status ="You can board the train" if balance >= train_fare  else "Insufficient balance"
-# print(status)
+# # print(status)
 
-# 3.Train Fare Category
+# # 3.Train Fare Category
 
-# age =int(input("Enter Your Age: "))
+# # age =int(input("Enter Your Age: "))
 
-# if 0<= age < 12:
-#     print("Child")
-# elif 12<= age <= 22:
-#     print("Student")
-# elif 23<= age <=64:
-#     print("Adult")
-# else:
-#     print("Senior")
+# # if 0<= age < 12:
+# #     # print("Child")
+# # elif 12<= age <= 22:
+# #     # print("Student")
+# # elif 23<= age <=64:
+# #     # print("Adult")
+# # else:
+# #     # print("Senior")
 
 # another way to solve 
 
@@ -74,8 +74,25 @@
 # else:
 #     print("Senior")
 
-# 04.Shinkansen Countdown
+# # 04.Shinkansen Countdown
 
-for i in range(10,0,-1):
-        print(i)
-print("Departing Tokyo Station!")
+# for i in range(10,0,-1):
+#         print(i)
+# print("Departing Tokyo Station!")
+
+# 05.ATM Withdrawal Attempts
+
+correct_pin =1234
+attemps = 3
+
+while attemps > 0:
+    users_pin =int(input("Enter Your pin: "))
+    if users_pin == correct_pin:
+        print("Access granted")
+        break
+    else:
+        attemps = attemps - 1
+        print(f"You have {attemps} attemps left")
+
+else:
+    print("Card blocked")
