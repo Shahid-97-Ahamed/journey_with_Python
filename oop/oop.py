6e9030044ebe5550a6c35b9ef93666b3b90ca72f
Shahid Ahamed <shahid.07ahamed@gmail.com>
2026-08-12 21:26:53 +0900

    Self Introduction Card

--- oop/oop.py
+++ oop/oop.py
@@ -1,27 +1,40 @@
-# class Airplains:
-#     def __init__(self,name,model,capacity,max_speed):
+# # class Airplains:
+# #     def __init__(self,name,model,capacity,max_speed):
+# #         self.name =name
+# #         self.model =model
+# #         self.capacity =capacity
+# #         self.max_speed =max_speed
+
+# # Bg=Airplains("BG616","767",456,215)
+# # Jal=Airplains("BG616","767",456,215)
+# # dubai=Airplains("BG616","767",456,215)
+
+# # print(Jal.capacity)
+
+
+# # --------------------------------------------------------------------------
+
+# class Students:
+#     def __init__(self,name,marks):
 #         self.name =name
-#         self.model =model
-#         self.capacity =capacity
-#         self.max_speed =max_speed
+#         self.marks =marks
 
-# Bg=Airplains("BG616","767",456,215)
-# Jal=Airplains("BG616","767",456,215)
-# dubai=Airplains("BG616","767",456,215)
+#     def get_avg(self):
+#         total =sum(self.marks)
+#         print(f"Hi {self.name},Your avarage markes is {total/len(self.marks):.1f}")
 
-# print(Jal.capacity)
+# my_result=Students("Shahid",[98,85,45,34])
+# my_result.get_avg()
 
 
-# --------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------
 
-class Students:
-    def __init__(self,name,marks):
-        self.name =name
-        self.marks =marks
+# Basic Fundamentan python Problems
 
-    def get_avg(self):
-        total =sum(self.marks)
-        print(f"Hi {self.name},Your avarage markes is {total/len(self.marks):.1f}")
+# 1.elf Introduction Card
 
-my_result=Students("Shahid",[98,85,45,34])
-my_result.get_avg()
+name ="Shahid"
+age = 32
+city ="Kagoshima"
+
+print(f"My name is {name}, I am {age} years old, and I live in {city}.")
